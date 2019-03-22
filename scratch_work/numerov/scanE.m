5c2c1d835915fa3de52b9a18ff5d8265674f3e6d
Jim Aman <jim.a.aman@gmail.com>
2019-03-22 07:21:07 -0500

    added a figure comparing increasing particle energies

--- scratch_work/numerov/scanE.m
+++ scratch_work/numerov/scanE.m
@@ -1,10 +1,31 @@
-numPnts = 5;
-[x, R] = deal(cell(1,numPnts));
-[S_el, del_el] = deal(zeros(1,numPnts));
+function solTable = scanE
 
-T = logspace(-1,-8,5);
-for i = 1:length(T)
+k        = @(mu, E) sqrt(2*mu*E);
+dBLambda = @(mu, E) 2*pi/k(mu, E);
+
+kel2Ha = 3.16683e-6; % conversion from Kelvin to Hartree
+amu2Au = 1.82289e3;  % conversion from amu to atomic units of mass
+
+% Input values
+T_inc  = 10.^(-1*(3:9)); % K
+masses = [84 84];
+el     = 4;
+
+% starting radius (haven't needed to tweak)
+startRad = 5;
+
+% plot output on same figure
+%figNum = figure;
+
+for i = 1:length(T_inc)
+    maxRad = 2*dBLambda((prod(masses)/sum(masses))*amu2Au, T_inc(i)*kel2Ha);
+    
     tic;
-    [x{i}, ~, R{i}, ~, S_el(i), del_el(i)] = sr_1S0_scatter(T(i), 0, [5 100e3], [84 84]);
+    out = sr_1S0_scatter(T_inc(i), el, [startRad maxRad], masses, 'region', [200 maxRad*0.1]);
     toc
+    
+    % I don't know how to preallocate tables in this version of Matlab.
+    % Later version seem to do it, use varfun(@class,t,'OutputFormat','cell') to get varTypes
+    if i == 1; solTable = out; else solTable = [solTable; out]; end
+end
 end
--- scratch_work/numerov/sr_1S0_scatter.m
+++ scratch_work/numerov/sr_1S0_scatter.m
@@ -25,6 +25,7 @@ function solOut = sr_1S0_scatter(Tinc, el, rRange, masses, varargin)
 %   coeffs  | (arb)       | Coefficients from normalizing the wavefunctions. This can save on propagation time once you've gotten the coefficients once.
 %   plot    | boolean     | flag for plotting
 %   figNum  | integer     | if plotting, use this figure number to plot on (useful for combining)
+%   region  | Bohr        | two element vector to specifying the region for plotting
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % OUTPUTS:
@@ -42,18 +43,11 @@ function solOut = sr_1S0_scatter(Tinc, el, rRange, masses, varargin)
 amu2Au = 1.82289e3;  % conversion from amu to atomic units of mass (see note above)
 kel2Ha = 3.16683e-6; % conversion from Kelvin to Hartree
 
-% Handy numbers which probably don't need to be messed with but better to group them together
-% than to have magic numbers down below
-minGuessRange = [0 10]; % Range to expect the potential minimum in, used for setting step size
-stepsPerOsc   = 12;     % Number of points per oscillation, more points = long run time. Solution may blow up if too small
-scatRegion    = 150;
-asymRegion    = 1e3;
-asymPntSkip   = 100; % In the asymptotic region, only plot every nth point (speeds up plot manipulation)
-
 % Specify default behavior (can be modified by varargin below)
 calcNorm = 1; % Asymptotic solution will be used to normalize the numerical solution (typically must propagate out to ~15e3 - 20e3 Bohr to ensure proper matching)
 flagPlot = 0; % Default to no plotting
-flagFig  = 0;
+flagFig  = 0; % Default to make a new figure
+region   = [150 500]; % Default plotting region for near-field and far-field scattering plots
 
 % Loop through specified optional parameters (if any)
 for i = 1:2:length(varargin)
@@ -66,8 +60,18 @@ for i = 1:2:length(varargin)
         case 'figNum'
             flagFig = 1;
             figNum  = varargin{i+1};
+        case 'region'
+            region  = varargin{i+1};
     end
 end
+
+% Handy numbers which probably don't need to be messed with but better to group them together
+% than to have magic numbers down below
+minGuessRange = [0 10]; % Range to expect the potential minimum in, used for setting step size
+stepsPerOsc   = 12;     % Number of points per oscillation, more points = long run time. Solution may blow up if too small
+scatRegion    = region(1); % Near-field region of scattering (only used for plotting)
+asymRegion    = region(2); % Far-field scattering region (only used for plotting)
+asymPntSkip   = 100; % In the asymptotic region, only plot every nth point (speeds up plot manipulation)
 
 
 %% Function definitions
@@ -78,17 +82,10 @@ funcV   = @(el, m, r) srPEC_1S0plus1S0(r)' + V_centr(el, m, r);
 %Wavevector definition
 k       = @(mu, E) sqrt(2*mu*E);
 
-% Analytic aymptotic solution
-posPlane = @(A, k, r) A*exp(1i*k.*r);
-negPlane = @(B, k, r) B*exp(-1i*k.*r);
-%fRAsym   = @(x, k, C, el) [ (-1i)^(el+1)*posPlane(C(1), k, x) (1i)^(el+1)*negPlane(C(2), k, x) ];
-funAsymR  = @(r, k, C, el) [ (C(1)/2)*besselh(el, 1, k.*r) (C(2)/2)*besselh(el, 2, k.*r) ]; % Sakurai 2nd Ed. - Eq. 6.4.49
-
-% Analytic aymptotic solution (different approach)
-inTrig = @(z, k, el) k.*z - el*pi/2;
-yAsym  = @(z, k, el, delL) (sin(inTrig(z, k, el)) + tan(delL)*cos(inTrig(z, k, el)));
-fDelL  = @(z, k, el, numY) atan((numY(2)*sin(inTrig(z(1), k, el)) - numY(1)*sin(inTrig(z(2), k, el)))/...
-                                (numY(1)*cos(inTrig(z(2), k, el)) - numY(2)*cos(inTrig(z(1), k, el))));
+% Analytic asymptotic solution
+ricBesJ    = @(x, el)  sqrt(pi*x/2).*besselj(el+1/2, x);
+ricBesY    = @(x, el) -sqrt(pi*x/2).*bessely(el+1/2, x);
+funRicBesR = @(r, k, C, el) [ C(1)*ricBesJ(k*r, el) -C(2)*ricBesY(k*r, el) ];
 
 % General Numerov propagator (used for linear second order differential equations)
 % INPUTS
@@ -126,26 +123,31 @@ for i = 3:length(r)
     % This is where the magic happens
     numR(i) = numerov(d, kLoc([i-2 i-1 i]).^2, numR([i-2 i-1]));
 end
+numR2 = numR;
 
 if calcNorm
-    % See Sakurai section 6.4 for discussion (particularly eq. 6.4.49 & .51)
-    % See also http://www.physics.udel.edu/~msafrono/425-2011/Lecture%2023.pdf for discussion
-    % and eq. 42-49 of http://www2.chem.umd.edu/groups/alexander/teaching/inelastic_scattering.pdf
+    % See Sakurai section 6.4 for discussion (particularly eq. 6.4.52)
+    % also eq. 41 of http://www2.chem.umd.edu/groups/alexander/teaching/inelastic_scattering.pdf
     
     % Find the value at the asymptotic solution to get normalization by enforcing (these are matricies)
-    % [ h_{el}^(1)(k*r_n)      h_{el}^(2)(k*r_n)     ] * [ A ] = [ R(r_n)/r_n        ]
-    % [ h_{el}^(1)(k*r_{n-1})  h_{el}^(2)(k*r_{n-1}) ]   [ B ]   [ R(r_{n-1}/r_{n-1} ]
-    C = funAsymR(r([end end-1])', kAsym, [1 1], el) \ (numR([end end-1])./r([end end-1]))';
+    % [ j_{el}(k*r_n)      y_{el}(k*r_n)     ] * [ A ] = [ R(r_n))    ]
+    % [ j_{el}(k*r_{n-1})  y_{el}(k*r_{n-1}) ]   [ B ]   [ R(r_{n-1}) ]
+    C = funRicBesR(r([end end-1])', kAsym, [1 1]', el) \ numR([end end-1])';
     C = C./norm(C); % overall magnitude shouldn't matter, so convenient to work around unity
 end
 
 % Find the phase shift
-S_el    = C(1)/C(2);
+del_el  = atan(C(2)/C(1));
+% single channel S matrix
+S_el    = exp(2i*del_el);
+% scattering amplitude
 f_el    = (S_el - 1)./(2i*kAsym);
-del_el  = real(log(S_el)/(2i));
 
-% Get the normalized wavefunctions
-asymR = sum(real(funAsymR(r', kAsym, C, el)), 2)'; 
+% Calculate the shifted and unshift asymptotic solutions
+asymR         = sum(real(funRicBesR(r', kAsym, C, el)), 2)';
+asymR_noShift = sum(real(funRicBesR(r', kAsym, [1 0], el)), 2)';
+
+% Normalize the numerical solution to the analytic value
 numR  = asymR(end)/numR(end)*numR;
 
 if flagPlot;
@@ -165,42 +167,93 @@ if flagPlot;
     axHan.TickLabelInterpreter = 'latex';
     legend({'V(r)', sprintf('$%g\\,+\\,%g, l = %g, \\frac{E_{inc}}{k_B} = %g\\times10^{%g}$ K',...
                             masses, el, Tinc/10^(floor(log10(Tinc))), floor(log10(Tinc)))},...
-        'Interpreter' , 'latex' )
+        'Interpreter' , 'latex' ,...
+        'Location'    , 'best'  )
     
     
-    axHan = subplot(2,1,2); hold on
+    axHan   = subplot(2,1,2); hold on
     asymInd = length(asymPart(asymPart == 0)); % index of the position cutoff
     plot(r(asymInd:asymPntSkip:end)./1e3, V(asymInd:asymPntSkip:end) ,...
          r(asymInd:asymPntSkip:end)./1e3, numR(asymInd:asymPntSkip:end) ,...
-         r(asymInd:asymPntSkip:end)./1e3, asymR(asymInd:asymPntSkip:end))
-    xlim([asymRegion r(end)]./1e3)
+         r(asymInd:asymPntSkip:end)./1e3, asymR(asymInd:asymPntSkip:end),...
+         r(asymInd:asymPntSkip:end)./1e3, asymR_noShift(asymInd:asymPntSkip:end))
+    xlim([asymRegion r(end)]./1e3); ylim([min(numR) max(numR)]*1.1)
     xlabel('Interparticle distance, r [$a_0\times10^3$]', 'Interpreter' , 'latex')
     ylabel('Asymptotic behavior (arb.)', 'Interpreter' , 'latex')
     box on
     axHan.FontSize = 22;
     axHan.YTick    = [];
     axHan.TickLabelInterpreter = 'latex';
-    legend({'V(r)', 'Numerical wavefunction', 'Asymp solution:\\$\frac{R(r)}{r} = C\left[S_{l}h_{l}^{1}(kr) + h_{l}^{2}(kr)\right]$'},...
-        'Interpreter' , 'latex' )
+    legend({'V(r)', 'Numerical wavefunction',...
+            '$R(r)_{\delta_l} = S_{l}\left[\cos{\delta_l}j_l(kr) - \sin{\delta_l}n_l(kr)\right]$',...
+            '$R(r)_{\delta_l=0} = j_l(kr)$'},...
+        'Interpreter' , 'latex' ,...
+        'Location'    , 'best'  )
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Try another appraoch to normalizing. Useful for debugging
-% This uses another form of the asymptotic solution, primarily to check that the
-% calculated phase shifts are the same.
-% del_el = fDelL(x([end end-1]), kAsym, el, R([end end-1]))
-% A    = yAsym(z(end), kAsym, el, del_el)/R(end);
-% R2   = A*R;
-%      z, 0.5*Rasym,...
-%      z, 0.5*sum(real(fRAsym(z', k(Einc), [1 1])), 2),...
-
+    
 %% Output
 % Build output table row (can be used to combine with other outputs)
-solOut = table(masses, Tinc, el, r, V, numR, asymR, del_el, S_el, f_el, {C});
+asymCoeffs = {C};
+% use structure here in order to combine with rows that have varying grid points
+outVecs.r = r;
+outVecs.PEC = V;
+outVecs.numR = numR;
+outVecs.asymR = asymR;
+solOut = table(masses, Tinc, el, outVecs, del_el, S_el, f_el, kAsym, asymCoeffs);
 
 %solOut.Properties.VariableNames        = {'masses', 'T_inc', 'el', 'r', 'PEC', 'numR', 'asymR', 'del_el', 'S_el', 'coeffs'};
-solOut.Properties.VariableDescriptions = {'input masses', 'input scattering energy', 'partial wave', 'interparticle spacing',...
-    '1S0 potential energy curve', 'numerical solution to R(r)', 'asymptotic solution to R(r)', 'phase shift for el partial wave',...
-    'single channel S matrix', 'partial wave amplitude for el', 'asymptotic coefficients'};
-solOut.Properties.VariableUnits        = {'a.m.u.', 'Kelvin', 'integer', 'a_0', 'Ha', 'arb', 'arb', 'radians', 'no dim', 'no dim', 'no dim'};
+solOut.Properties.VariableDescriptions = {'input masses', 'input scattering energy', 'partial wave', 'structure of output curves',...
+    'phase shift for el partial wave', 'single channel S matrix', 'partial wave amplitude for el', 'collision wavevector','asymptotic coefficients'};
+solOut.Properties.VariableUnits        = {'a.m.u.', 'Kelvin', 'integer', 'no dim', 'radians', 'no dim', 'no dim', '1/a_0','no dim'};
 end
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Another appraoch to normalizing. Useful for debugging
+% % This uses another form of the asymptotic solution
+% 
+% % Analytic aymptotic solution (different approach)
+% inTrig    = @(z, k, el) k.*z - el*pi/2;
+% funTrigR  = @(z, k, el, delL) (sin(inTrig(z, k, el)) + tan(delL)*cos(inTrig(z, k, el)));
+% fDelL     = @(z, k, el, numY) atan((numY(2)*sin(inTrig(z(1), k, el)) - numY(1)*sin(inTrig(z(2), k, el)))/...
+%                                    (numY(1)*cos(inTrig(z(2), k, el)) - numY(2)*cos(inTrig(z(1), k, el))));
+% 
+% del_el2 = fDelL(r([end end-1]), kAsym, el, numR2([end end-1]));
+% 
+% % Get the normalized wavefunctions
+% asymR2 = funTrigR(r, kAsym, el, del_el2);
+% asymR3 = funTrigR(r, kAsym, el, 0);
+% numR2  = funTrigR(r(end), kAsym, el, del_el2)/numR2(end)*numR2;
+%     
+%     if flagFig; figure(figNum); else figure; end
+%     axHan = subplot(2,1,1); hold on
+%     plot(r(scatPart), V(scatPart) , r(scatPart), numR2(scatPart))
+%     xlim([0 scatRegion]);
+%     ylim([min(numR2(scatPart)) max(numR2(scatPart))]*1.1)
+%     xlabel('Interparticle distance, r [$a_0$]', 'Interpreter' , 'latex')
+%     ylabel('Wavefunction (arb.)', 'Interpreter' , 'latex');
+%     box on
+%     axHan.FontSize = 22;
+%     axHan.YTick    = [];
+%     axHan.TickLabelInterpreter = 'latex';
+%     legend({'V(r)', sprintf('$%g\\,+\\,%g, l = %g, \\frac{E_{inc}}{k_B} = %g\\times10^{%g}$ K',...
+%                             masses, el, Tinc/10^(floor(log10(Tinc))), floor(log10(Tinc)))},...
+%         'Interpreter' , 'latex' )
+%     
+%     
+%     axHan = subplot(2,1,2); hold on
+%     asymInd = 1; % index of the position cutoff
+%     asymPntSkip = 1;
+%     plot(r(asymInd:asymPntSkip:end)./1e3, V(asymInd:asymPntSkip:end) ,...
+%          r(asymInd:asymPntSkip:end)./1e3, numR2(asymInd:asymPntSkip:end) ,...
+%          r(asymInd:asymPntSkip:end)./1e3, asymR2(asymInd:asymPntSkip:end) ,...
+%          r(asymInd:asymPntSkip:end)./1e3, asymR3(asymInd:asymPntSkip:end))
+%     xlim([0 r(end)]./1e3)
+%     xlabel('Interparticle distance, r [$a_0\times10^3$]', 'Interpreter' , 'latex')
+%     ylabel('Asymptotic behavior (arb.)', 'Interpreter' , 'latex')
+%     box on
+%     axHan.FontSize = 22;
+%     axHan.YTick    = [];
+%     axHan.TickLabelInterpreter = 'latex';
+%     legend({'V(r)', 'Numerical wavefunction', 'Asymp solution:\\$\frac{R(r)}{r} = C\left[S_{l}h_{l}^{1}(kr) + h_{l}^{2}(kr)\right]$'},...
+%         'Interpreter' , 'latex' )
