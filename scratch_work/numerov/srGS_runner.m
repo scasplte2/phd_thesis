bf19ad3e71ba9158496a15be6ebbde911dafbe96
Jim Aman <jim.a.aman@gmail.com>
2019-04-02 17:36:55 -0500

    finished up toy model plots and characterization

--- scratch_work/numerov/srGS_runner.m
+++ scratch_work/numerov/srGS_runner.m
@@ -1,8 +1,10 @@
 % Use atomic units throughout 
 
 %% Physical definitions
-amu2Au = 1822.889;  % conversion from amu to atomic units of mass (see note above)
-kel2Ha = 3.16683e-6; % conversion from Kelvin to Hartree
+amu2Au = 1822.889;     % conversion from amu to atomic units of mass (see note above)
+kel2Ha = 3.16683e-6;   % conversion from Kelvin to Hartree
+cm2Ha  = 4.55634e-6;
+aBohr  = 0.52917721067;
 
 % Strontium masses
 % should use these for accurate measurements, even these slight differences can
@@ -19,23 +21,26 @@ dBLambda = @(mu, E) 2*pi/k(mu, E);
 % Useful potential forms
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %C = [ 3.164315349415183e+03 3.753373129228250e+05 5.226574577253875e+07 7.435*1e9  ]; % Large negative (84+84)
-C = [ 3.164315349415183e+03 3.753373129228250e+05 5.226574577253875e+07 7.4345*1e9 ]; % Large positive (84+84)
+%C = [ 3.164315349415183e+03 3.753373129228250e+05 5.226574577253875e+07 7.4345*1e9 ]; % Large positive (84+84)
+%mod_LJ = @(r) C(4)./r.^12 - C(1)./r.^6 - C(2)./r.^8 - C(3)./r.^10;
 
-mod_LJ = @(r) C(4)./r.^12 - C(1)./r.^6 - C(2)./r.^8 - C(3)./r.^10;
+C = [948*cm2Ha 1/aBohr];
+LJ = @(r) C(1)*( (C(2)./r).^12 - 2*(C(2)./r).^6 );
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Problem specifics
-T    = 1e-9; 
+T    = 10e-9; 
 mass = [m86 m86];
 
 maxRad = 2*dBLambda(prod(mass)/sum(mass)*amu2Au, T*kel2Ha); 
-range1 = 1500;
+range1 = maxRad/1000; % 1500 for 86 real potential, default to 150 for 84
 range2 = maxRad/10;
 
 %% Evaluate wavefunction
 tic; 
-t = sr_1S0_scatter(T, 0, [5 maxRad], mass, 'plot', 1, 'region', [range1 range2]); 
-%t = sr_1S0_scatter(T, 0, [5 maxRad], mass, 'plot', 1, 'region', [range1 range2], 'funcPEC', mod_LJ); 
+%t = sr_1S0_scatter(T, 0, [5 maxRad], mass, 'plot', 1, 'region', [range1 range2]); 
+%t = sr_1S0_scatter(T, 0, [5 maxRad], mass, 'plot', 1, 'region', [range1 range2], 'funcPEC', mod_LJ);
+t = sr_1S0_scatter(T, 0, [0.1 maxRad], mass, 'plot', 1, 'region', [range1 range2], 'funcPEC', LJ); 
 toc
 
 %% Estimate scattering length
